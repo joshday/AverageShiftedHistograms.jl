f39719892867e89caf4f839d8629bdef6958a3b9
Josh Day <emailjoshday@gmail.com>
2015-06-30 19:46:59 -0400

    rename plot.jl to gadflymethods.jl.  Allow for plot methods from other packages in the future.  More testing

--- src/AverageShiftedHistograms.jl
+++ src/AverageShiftedHistograms.jl
@@ -5,9 +5,18 @@ module AverageShiftedHistograms
     import Base: quantile, merge, merge!, copy, mean, var, std
     import Compat: @compat
     import StatsBase:
-        fit, nobs, Histogram, AbstractHistogram, midpoints, WeightVec
+        fit, nobs, Histogram, AbstractHistogram, WeightVec, midpoints
 
-    export UnivariateASH, updatebatch!, fit, ash
+    export UnivariateASH, updatebatch!, fit, ash!, midpoints, loadvis
 
     include("univariateash.jl")
+
+
+    # Hack to avoid loading Gadfly each time (adopted from Sigma.jl)
+    function loadvis(package::Symbol = :Gadfly)
+        gadflypath = Pkg.dir("AverageShiftedHistograms", "src", "gadflymethods.jl")
+        package == :Gadfly && include(gadflypath)
+        # TODO: Add methods for other plotting packages
+    end
+
 end
