6d3437b7cd6ebc532a028ce819de23c3933d160f
autodocs <autodocs>
2018-05-22 18:28:44 +0000

    build based on a0c14cd

--- latest/search_index.js
+++ latest/search_index.js
@@ -20,7 +20,7 @@ var documenterSearchIndex = {"docs": [
     "location": "index.html#AverageShiftedHistograms.ash",
     "page": "AverageShiftedHistograms.jl",
     "title": "AverageShiftedHistograms.ash",
-    "category": "Function",
+    "category": "function",
     "text": "Univariate Ash\n\nash(x; kw...)\n\nFit an average shifted histogram to data x.  Keyword options are:\n\nrng    : values over which the density will be estimated\nm      : Number of adjacent histograms to smooth over\nkernel : kernel used to smooth the estimate\n\nBivariate Ash\n\nash(x, y; kw...)\n\nFit a bivariate averaged shifted histogram to data vectors x and y.  Keyword options are:\n\nrngx    : x values where density will be estimated\nrngy    : y values where density will be estimated\nmx      : smoothing parameter in x direction\nmy      : smoothing parameter in y direction\nkernelx : kernel in x direction\nkernely : kernel in y direction\n\nMutating an Ash object\n\nAsh objectes can be updated with new data, smoothing parameter(s), or kernel(s).  They cannot, however, change the ranges over which the density is estimated.  It is therefore suggested to err on the side of caution when choosing data endpoints.\n\n# univariate\nash!(obj; kw...)\nash!(obj, newx, kw...)\n\n# bivariate\nash!(obj; kw...)\nash!(obj, newx, newy; kw...)\n\n\n\n"
 },
 
@@ -52,7 +52,7 @@ var documenterSearchIndex = {"docs": [
     "location": "index.html#AverageShiftedHistograms.Kernels",
     "page": "AverageShiftedHistograms.jl",
     "title": "AverageShiftedHistograms.Kernels",
-    "category": "Module",
+    "category": "module",
     "text": "The Kernels module defines a collection of kernels to avoid namespace collisions:\n\nbiweight\ncosine\nepanechnikov\ntriangular\ntricube\ntriweight\nuniform\ngaussian\nlogistic\n\n\n\n"
 },
 
@@ -76,7 +76,7 @@ var documenterSearchIndex = {"docs": [
     "location": "api.html#AverageShiftedHistograms.ash!-Tuple{AverageShiftedHistograms.Ash}",
     "page": "API",
     "title": "AverageShiftedHistograms.ash!",
-    "category": "Method",
+    "category": "method",
     "text": "ash!(o::Ash; kw...)\nash!(o::Ash, newdata; kw...)\n\nUpdate an Ash estimate with new data, smoothing parameter (keyword m), or kernel (keyword kernel):\n\n\n\n"
 },
 
@@ -84,7 +84,7 @@ var documenterSearchIndex = {"docs": [
     "location": "api.html#AverageShiftedHistograms.ash-Tuple{AbstractArray}",
     "page": "API",
     "title": "AverageShiftedHistograms.ash",
-    "category": "Method",
+    "category": "method",
     "text": "Univariate Ash\n\nash(x; kw...)\n\nFit an average shifted histogram to data x.  Keyword options are:\n\nrng    : values over which the density will be estimated\nm      : Number of adjacent histograms to smooth over\nkernel : kernel used to smooth the estimate\n\nBivariate Ash\n\nash(x, y; kw...)\n\nFit a bivariate averaged shifted histogram to data vectors x and y.  Keyword options are:\n\nrngx    : x values where density will be estimated\nrngy    : y values where density will be estimated\nmx      : smoothing parameter in x direction\nmy      : smoothing parameter in y direction\nkernelx : kernel in x direction\nkernely : kernel in y direction\n\nMutating an Ash object\n\nAsh objectes can be updated with new data, smoothing parameter(s), or kernel(s).  They cannot, however, change the ranges over which the density is estimated.  It is therefore suggested to err on the side of caution when choosing data endpoints.\n\n# univariate\nash!(obj; kw...)\nash!(obj, newx, kw...)\n\n# bivariate\nash!(obj; kw...)\nash!(obj, newx, newy; kw...)\n\n\n\n"
 },
 
@@ -92,7 +92,7 @@ var documenterSearchIndex = {"docs": [
     "location": "api.html#AverageShiftedHistograms.extendrange",
     "page": "API",
     "title": "AverageShiftedHistograms.extendrange",
-    "category": "Function",
+    "category": "function",
     "text": "extendrange(x, s = .5, n = 200)\n\nCreate a LinSpace of length n starting at s standard deviations below minimum(x) and ending at s standard deviations above maximum(x)\n\n\n\n"
 },
 
@@ -100,7 +100,7 @@ var documenterSearchIndex = {"docs": [
     "location": "api.html#AverageShiftedHistograms.nout-Tuple{AverageShiftedHistograms.Ash}",
     "page": "API",
     "title": "AverageShiftedHistograms.nout",
-    "category": "Method",
+    "category": "method",
     "text": "return the number of observations that fell outside of the histogram range\n\n\n\n"
 },
 
@@ -108,7 +108,7 @@ var documenterSearchIndex = {"docs": [
     "location": "api.html#AverageShiftedHistograms.xy-Tuple{AverageShiftedHistograms.Ash}",
     "page": "API",
     "title": "AverageShiftedHistograms.xy",
-    "category": "Method",
+    "category": "method",
     "text": "return the range and density of a univariate ASH\n\n\n\n"
 },
 
@@ -116,7 +116,7 @@ var documenterSearchIndex = {"docs": [
     "location": "api.html#AverageShiftedHistograms.xyz-Tuple{AverageShiftedHistograms.Ash2}",
     "page": "API",
     "title": "AverageShiftedHistograms.xyz",
-    "category": "Method",
+    "category": "method",
     "text": "return ranges and density of biviariate ASH\n\n\n\n"
 },
 
@@ -124,7 +124,7 @@ var documenterSearchIndex = {"docs": [
     "location": "api.html#Distributions.cdf-Tuple{AverageShiftedHistograms.Ash,Real}",
     "page": "API",
     "title": "Distributions.cdf",
-    "category": "Method",
+    "category": "method",
     "text": "cdf(o::Ash, x::Real)\n\nReturn the estimated cumulative density at the point x.\n\n\n\n"
 },
 
@@ -132,7 +132,7 @@ var documenterSearchIndex = {"docs": [
     "location": "api.html#Distributions.pdf-Tuple{AverageShiftedHistograms.Ash,Real}",
     "page": "API",
     "title": "Distributions.pdf",
-    "category": "Method",
+    "category": "method",
     "text": "pdf(o::Ash, x::Real)\n\nReturn the estimated density at the point x.\n\n\n\n"
 },
 
@@ -140,7 +140,7 @@ var documenterSearchIndex = {"docs": [
     "location": "api.html#StatsBase.nobs-Tuple{AverageShiftedHistograms.Ash}",
     "page": "API",
     "title": "StatsBase.nobs",
-    "category": "Method",
+    "category": "method",
     "text": "return the number of observations\n\n\n\n"
 },
 
@@ -148,7 +148,7 @@ var documenterSearchIndex = {"docs": [
     "location": "api.html#AverageShiftedHistograms.histdensity-Tuple{AverageShiftedHistograms.Ash}",
     "page": "API",
     "title": "AverageShiftedHistograms.histdensity",
-    "category": "Method",
+    "category": "method",
     "text": "return the histogram values as a density (intergrates to 1)\n\n\n\n"
 },
 
