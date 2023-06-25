bece9cb235dbd7ccb851cec8c530918b233ac393
Josh Day <emailjoshday@gmail.com>
2023-06-24 20:10:59 -0400

    updating code to current personal preferences

--- src/kernels.jl
+++ src/kernels.jl
@@ -1,14 +1,13 @@
 module Kernels
-in_range(u) = abs(u) ≤ 1
+biweight(u)         = abs(u) ≤ 1 ? (1.0 - u ^ 2) ^ 2 : 0.0
+cosine(u)           = abs(u) ≤ 1 ? cos(0.5 * π * u) : 0.0
+epanechnikov(u)     = abs(u) ≤ 1 ? 1.0 - u ^ 2 : 0.0
+triangular(u)       = abs(u) ≤ 1 ? 1.0 - abs(u) : 0.0
+tricube(u)          = abs(u) ≤ 1 ? (1.0 - abs(u) ^ 3) ^ 3 : 0.0
+triweight(u)        = abs(u) ≤ 1 ? (1.0 - u ^ 2) ^ 3 : 0.0
+uniform(u)          = abs(u) ≤ 1 ? 0.5 : 0.0
 
-biweight(u)         = in_range(u) ? (1.0 - u ^ 2) ^ 2 : 0.0
-cosine(u)           = in_range(u) ? cos(0.5 * π * u) : 0.0
-epanechnikov(u)     = in_range(u) ? 1.0 - u ^ 2 : 0.0
-triangular(u)       = in_range(u) ? 1.0 - abs(u) : 0.0
-tricube(u)          = in_range(u) ? (1.0 - abs(u) ^ 3) ^ 3 : 0.0
-triweight(u)        = in_range(u) ? (1.0 - u ^ 2) ^ 3 : 0.0
-uniform(u)          = in_range(u) ? 0.5 : 0.0
-
+# Kernels that don't require -1 ≤ u ≤ 1.
 gaussian(u) = exp(-0.5 * u ^ 2)
 logistic(u) = 1.0 / (exp(u) + 2.0 + exp(-u))
 end
